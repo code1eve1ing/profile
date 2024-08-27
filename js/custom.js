caf0d7499da68ae412b4a4832e9ea4b14b5094fd
Sanket Mistry <sanket.mern.dev@gmail.com>
2024-08-27 10:43:04 +0530

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,43 @@
+(function ($) {
+
+  "use strict";
+
+  // COLOR MODE
+  $('.color-mode').click(function () {
+    $('.color-mode-icon').toggleClass('active')
+    $('body').toggleClass('dark-mode')
+  })
+
+  // HEADER
+  $(".navbar").headroom();
+
+  // PROJECT CAROUSEL
+  $('.owl-carousel').owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 10,
+    nav: true
+  });
+
+  // Skills
+  $('.skills').waypoint(function () {
+    $('.progress .progress-bar').each(function () {
+      $(this).css("width", $(this).attr("aria-valuenow") + '%');
+    });
+  }, { offset: '80%' });
+
+  // SMOOTHSCROLL
+  $(function () {
+    $('.nav-link, .custom-btn-link').on('click', function (event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top - 49
+      }, 1000);
+      event.preventDefault();
+    });
+  });
+
+  // TOOLTIP
+  $('.social-links a').tooltip();
+
+})(jQuery);
